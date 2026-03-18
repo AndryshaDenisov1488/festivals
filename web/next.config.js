909f35166b3bebbc1ebcc924b71c24f46a598ce3
Andrysha1488 <fylhtq255082@gmail.com>
2026-03-18 13:07:01 +0300

    фикс

--- web/next.config.js
+++ web/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ''
 const nextConfig = {
-  output: 'standalone'
+  output: 'standalone',
+  basePath: basePath || undefined,
+  assetPrefix: basePath ? `${basePath}/` : undefined
 }
 
 module.exports = nextConfig
